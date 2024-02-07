f070bb5e4968984e2a60336d2047d574a2291f53
Eric Stock <eric.stock@K202Domain.CSN.com>
2024-02-07 11:54:29 -0330

    Setup nav and form to update data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { useState } from "react";
 
 import './App.css';
-import Bicycle from './Bicycle';
+import Bicycle, {UpdateBicycle} from './Bicycle';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+
+  const [bikeInfo, setBikeInfo] = useState({ name: "Red Bike", color: "Red", image: "RedBike.png"});
+
   return (
-    <Bicycle/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Bicycle info={bikeInfo} setInfo={setBikeInfo}/>}/>
+        <Route path="/update" element={<UpdateBicycle setInfo={setBikeInfo}/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/Bicycle.js
+++ src/Bicycle.js
@@ -1,11 +1,60 @@
+import NavBar from "./Navigation.js";
+import { useRef } from "react";
 
 
-function Bicycle() {
+function Bicycle(props) {
+    
+    const submit = (e) => {
+        e.preventDefault(); 
+        props.info.name = "Blue Bike"; 
+        props.setInfo({ name: "Bike", color: "Yellow", image: "YellowBike.png"}); 
+        console.log(props.info);
+    }
+    
     return (
         <>
-            <h2>Red Bike</h2>
-            <h3>Red</h3>
-            <img src = "images/RedBike.png"></img>
+            <NavBar/>
+            <h2>{props.info.name}</h2>
+            <h3>{props.info.color}</h3>
+            <img src = {"images/"+props.info.image}></img>
+            <form onSubmit={submit}>
+              <button>Change</button>  
+            </form>
+        </>
+    )
+}
+
+
+
+export function UpdateBicycle(props) {
+    const nameText = useRef();
+    const colorText = useRef();
+    const imageText = useRef();
+    
+    const submit = (e) => {
+        e.preventDefault();
+        props.setInfo( {name: nameText.current.value, color: colorText.current.value, image: imageText.current.value});
+    }
+    
+    return (
+        <>
+        <NavBar/>
+        <h2>Update Bicycle</h2>
+        <form onSubmit={ submit }  >
+            <input 
+                type="text"
+                ref={ nameText }
+                placeholder="enter name..."/>
+            <input
+                type="text"
+                ref={colorText}
+                placeholder="enter color..."/>
+            <input
+                type="text"
+                ref={imageText}
+                placeholder="enter image..."/>
+            <button>Update</button>
+        </form>
         </>
     )
 }
