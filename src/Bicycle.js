7c084764d1ec03cad29945eee0254283bfef8e68
Eric Stock <eric.stock@K202Domain.CSN.com>
2024-02-07 10:54:24 -0330

    Setup initial bicycle display

---
+++ src/Bicycle.js
@@ -0,0 +1,13 @@
+
+
+function Bicycle() {
+    return (
+        <>
+            <h2>Red Bike</h2>
+            <h3>Red</h3>
+            <img src = "images/RedBike.png"></img>
+        </>
+    )
+}
+
+export default Bicycle;
