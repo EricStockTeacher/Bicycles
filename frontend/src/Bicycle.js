f0612481b7913dcfceeae87c85cfa4ae709f4720
EricStockTeacher <ericstockteacher@gmail.com>
2024-02-09 10:42:09 -0330

    added backend to server react front end and api routes for getting and modifying bicycle data, setup proxy on front end

--- frontend/src/Bicycle.js
+++ frontend/src/Bicycle.js
@@ -1,30 +1,43 @@
 import NavBar from "./Navigation.js";
 import { useRef } from "react";
-
+import { useEffect } from "react";
 
 function Bicycle(props) {
     
-    const submit = (e) => {
-        e.preventDefault(); 
-        props.info.name = "Blue Bike"; 
-        props.setInfo({ name: "Bike", color: "Yellow", image: "YellowBike.png"}); 
-        console.log(props.info);
-    }
-    
+    useEffect( () => {
+        var myHeaders = new Headers();
+        var requestOptions = {
+            method: 'GET',
+            headers: myHeaders,
+            redirect: 'follow'
+        };
+
+        fetch("/api/bicycle", requestOptions)
+        .then(
+            response =>  {
+                if( !response.ok) {
+                    let code = response.status.toString();
+                    throw new Error( `${code} ${response.statusText}`);
+                }
+                return response.json();
+        })
+        .then( bike => props.setInfo(bike) )
+        .catch( e => {
+            console.log("Error!!!");
+            console.log(e.message);
+        });
+        
+      },[])
+
     return (
         <>
             <NavBar/>
             <h2>{props.info.name}</h2>
             <h3>{props.info.color}</h3>
             <img src = {"images/"+props.info.image}></img>
-            <form onSubmit={submit}>
-              <button>Change</button>  
-            </form>
         </>
     )
 }
-
-
 
 export function UpdateBicycle(props) {
     const nameText = useRef();
@@ -32,25 +45,28 @@ export function UpdateBicycle(props) {
     const imageText = useRef();
     
     const submit = (e) => {
-        e.preventDefault();
-        props.setInfo( {name: nameText.current.value, color: colorText.current.value, image: imageText.current.value});
+        //e.preventDefault();
+        //props.setInfo( {name: nameText.current.value, color: colorText.current.value, image: imageText.current.value});
     }
     
     return (
         <>
         <NavBar/>
         <h2>Update Bicycle</h2>
-        <form onSubmit={ submit }  >
+        <form action ='/api/updateBicycle' method="post"  onSubmit={ submit }  >
             <input 
                 type="text"
+                name ="name"
                 ref={ nameText }
                 placeholder="enter name..."/>
             <input
                 type="text"
+                name="color"
                 ref={colorText}
                 placeholder="enter color..."/>
             <input
                 type="text"
+                name="image"
                 ref={imageText}
                 placeholder="enter image..."/>
             <button>Update</button>
