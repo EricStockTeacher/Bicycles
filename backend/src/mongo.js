701377cb61b565cdf27b3351fc76ed23258906c4
Eric Stock <eric.stock@K202Domain.CSN.com>
2024-02-16 14:04:20 -0330

    removed mongo connect url and fixed nodemon

--- backend/src/mongo.js
+++ backend/src/mongo.js
@@ -3,7 +3,6 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
 const uri = process.env.MONGO_URI;
-//"mongodb+srv://ericstock:urJkxr3c7ccBxyUA@cluster0.fan0a.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri,  {
