f151ed7e55d19f82687b1574e76c89f14a4fe977
EricStockTeacher <ericstockteacher@gmail.com>
2024-02-19 10:44:11 -0330

    Added lambda functions scripts for backend

---
+++ backend-lambda/getBicycles.js
@@ -0,0 +1,38 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+export const handler = async (event) => {
+  const uri = process.env.MONGO_URI;
+  
+  // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+  const client = new MongoClient(uri,  {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    }
+  );
+  
+  try {
+    await client.connect();
+  
+    const database = client.db("bicycle-store");
+    const bikes = database.collection("bike");
+    
+    const updateDoc = {
+            $set: {
+                name: event.name,
+                color: event.color,
+                image: event.image
+            },
+        };
+    const result = await bikes.updateOne({}, updateDoc);
+    console.log(result);
+
+    return event;
+  }
+  finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
