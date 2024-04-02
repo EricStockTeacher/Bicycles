38bf591f37df338c9dd1f2376cd5fe6d17b20a3c
Eric Stock <eric.stock@K202Domain.CSN.com>
2024-04-02 14:25:52 -0230

    Finished implementing mongoose

--- backend/src/bikeModel.js
+++ backend/src/bikeModel.js
@@ -2,10 +2,10 @@ import mongoose from 'mongoose';
 const { Schema, model } = mongoose;
 
 const bikeSchema = new Schema({
-  name: String,
+  name: { type: String, required: true },
   color: String,
   image: String,
-  email: String
+  email: { type: String, required: true }
 });
 
 const bikeModel = model('bike', bikeSchema);
